36abf6123f00d1d8a3a8dc9e7f1e0a3a7d82d193
Dananando <dan.andriamihaja@gmail.com>
2021-09-27 15:05:42 +0200

    Last commit - very high level comments on what I would have liked to do

--- Comments.md
+++ Comments.md
@@ -1,1 +1,5 @@
 # My comments following the exercise
+
+- I got stuck from the start
+- I tried tackling the exercise by making the loops in the DomainFilter.component more readable : this proved too hard for me
+- The exercise and Sipios' requirements may just be too high for me. Simple as that.
--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -20,25 +20,33 @@ class DomainFilter extends React.Component<Props, State> {
       subClassifications: []
     }
 
-    const s: any = {};
+    // Not useful?
+    // const s: any = {};
 
-    for(let i = 0; i < domains.length; i++) {
-      if (this.state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        this.state.countries.push(domains[i].substring(0,2))
+    // Modify loop to make it clearer - Not understandable enough
+    for (let i = 0; i < domains.length; i++) {
+      if (this.state.countries.indexOf(domains[i].substring(0, 2)) <= 0) {
+        this.state.countries.push(domains[i].substring(0, 2));
+        console.log(this.state.countries);
       }
-      this.state.classifications.push(domains[i].substring(3,5));
+      this.state.classifications.push(domains[i].substring(3, 5));
       let flag = false;
-      for(let j = 0; j < this.state.subClassifications.length; j++) {
+
+      // Modify loop to make it clearer as well
+      for (let j = 0; j < this.state.subClassifications.length; j++) {
         if (this.state.subClassifications[j] == domains[i].substring(6)) {
           flag = true
+          console.log('With flag', this.state.subClassifications);
           break;
         }
       }
       if (!flag) {
         this.state.subClassifications.push(domains[i].substring(6));
+        console.log('No flag', this.state.subClassifications);
       }
     }
 
+    // Clarify parameters of the filter
     this.setState({
       ...this.state,
       classifications: this.state.classifications.filter((e, i, l) => l.indexOf(e) === i),
@@ -47,7 +55,7 @@ class DomainFilter extends React.Component<Props, State> {
   }
 
   render() {
-    const {countries, classifications, subClassifications} = this.state || {
+    const { countries, classifications, subClassifications } = this.state || {
       countries: [],
       classifications: [],
       subClassifications: []
